dfea0e5723056a718ca7b167142696fe9fb4b44c
Teodor Savin <t.savin@global.leaseweb.com>
2019-08-23 10:50:12 +0200

    readme

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  igdbBaseUrl: 'http://127.0.0.1:8000',
-  apiKey: '7238da3102951d990afdde3c7a7f10b2'
+  igdbBaseUrl: 'http://127.0.0.1:8000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  igdbBaseUrl: 'http://127.0.0.1:8000',
-  apiKey: '7238da3102951d990afdde3c7a7f10b2'
+  igdbBaseUrl: 'http://127.0.0.1:8000'
 };
 
 /*
